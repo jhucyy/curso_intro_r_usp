46e3653c61c49ab80880daf2c7101ca62a314041
jhucyy <jhucyy.pereira@gmail.com>
2022-07-08 13:26:34 -0300

    introdução

---
+++ introducao.R
@@ -0,0 +1,34 @@
+
+# Introdução --------------------------------------------------------------
+
+# O curso tem como objetivo auxiliar estudantes da
+# pós a desenvolver habilidades computacionais, que
+# permitam a elobaração de um projeto, desde a fase de
+# criação do primeiro script para a extração dos dados
+# até a limpeza dos dados, data wrangling, para se
+# se chegar ao ideal tidy
+# e por fim a estruturação de gráficos
+# e fomentação de análises claras e objetivas sobre os dados.
+
+
+
+# Objetivos a serem alcançados --------------------------------------------
+
+# O curso se estrutura em 4 princípios
+# 1 - Transparência:
+# toda a análise pode ser inspecionada, 
+# e as origens dos erros podem ser identificados.
+
+# 2 - Simplicidade:
+# digitar os códigos da forma mais simples e 
+# clara possível, para que seja 
+# de fácil leitura e compreensão.
+
+# 3 - Reprodutibilidade:
+# Outras pessoas devem ser capaz de 
+# reproduzir aquilo que fizemos no nosso código.
+
+# 4 - Entender os dados:
+# nada disto terá sentido, se ao final do processo
+# não for possível despreender análises a partir 
+# da base de dados extraída.
