7d63022c6b508bd64741ed857e062c52e78f45f1
jhucyy <jhucyy.pereira@gmail.com>
2022-07-26 15:21:48 -0300

    continuação

---
+++ aula_2_1.R
@@ -0,0 +1,11 @@
+
+# Aula 2.1 ----------------------------------------------------------------
+
+# link da aula
+# https://jonnyphillips.github.io/Ciencia_de_Dados/abrindo_manipulando.html
+
+## parei aqui:
+# Controlando o que Aparece no Relatório Final
+
+# link do video
+# https://drive.google.com/file/d/1HxeqxZolQH8ZnAZUCv5KmfS_u0KBDmMO/view
