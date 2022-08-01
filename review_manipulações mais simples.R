1733389a70351b292a2a7a3d40db0e0f452ff9fd
Juciane <jhucyy.pereira@gmail.com>
2022-08-01 10:00:53 -0300

    review manipulação simples de dados

--- review_manipulações mais simples.R
+++ review_manipulações mais simples.R
@@ -1,4 +1,24 @@
 
 # Review - manipulações mais simples no R ---------------------------------
 
+# Renomeando variáveis:
+
+
+library(tidyverse)
+library(nycflights13)
+flights %>% 
+  rename("departure_time" = dep_time)
+
+
+## Filtrar com base em mês e dia
+
+
+flights %>% 
+  filter(month == 1 & day ==2)
+
+
+## Abrindo bancos de dados:
+
+
+mortalidade_infantil <- read_csv("./mortalidade-infantil-óbitos-a-cada-mil-nascidos-vivos.csv")
 
