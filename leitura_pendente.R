4c2ebffd29c1b3ed6c6b5a580eb7192906db29b4
Juciane <jhucyy.pereira@gmail.com>
2022-08-28 17:03:55 -0300

    leitura pendente para avançar para tutorial 6

---
+++ leitura_pendente.R
@@ -0,0 +1,6 @@
+
+# Leitura pendente --------------------------------------------------------
+
+# Antes da próxima aula, por favor leia R 4 Data Science, Capítulo 3 
+# e ggplot2: Elegant Graphics for Data Analysis, Capítulos 1-5
+
