9e9c00236e5ce56df122a182090bdb7d80f47c46
Juciane <jhucyy.pereira@gmail.com>
2022-09-07 14:03:27 -0300

    .

--- aulas/anotacoes_graficos.R
+++ aulas/anotacoes_graficos.R
@@ -9,3 +9,33 @@
 # Carregando o pacote: ----------------------------------------------------
 
 library(tidyverse)
+
+mpg
+
+# Plotando um gráfico: ----------------------------------------------------
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+
+
+# Modelo exemplar de um gráfico no ggplot2 --------------------------------
+# 
+# ggplot(data = <DATA>) + 
+#   <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
+
+
+
+# Plotando outro gráfico --------------------------------------------------
+
+diamonds
+
+
+ggplot(data = diamonds)+
+  geom_col(mapping = aes(x = cut, y = price))+
+  coord_flip()
+
+
+
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = hwy, y = cyl))
