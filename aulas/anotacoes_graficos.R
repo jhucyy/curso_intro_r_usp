80ccd2721080d462d2abe292567d859ff837374d
Juciane <jhucyy.pereira@gmail.com>
2022-09-07 13:35:17 -0300

    .

---
+++ aulas/anotacoes_graficos.R
@@ -0,0 +1,11 @@
+
+# Visualização de dados ---------------------------------------------------
+
+# Para tabalhar com gráficos iremos utilizar o pacote ggplot2 presente no 
+# meta pacote tidyverse.
+
+
+
+# Carregando o pacote: ----------------------------------------------------
+
+library(tidyverse)
